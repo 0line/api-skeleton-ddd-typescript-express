ad8b6552b4f1aa611662b5348091297beca098fe
0line <jesuslozano.red@gmail.com>
2023-07-21 00:18:24 -0600

    add  unit test

---
+++ src/Context/Register/Infraestructure/Persistense/FileUserRepository.ts
@@ -0,0 +1,39 @@
+import { deserialize, serialize } from 'bson';
+import { User } from "../../Domain/User";
+import { UserRepository } from "../../Domain/UserRepository";
+import  fs from 'fs';
+
+export class FileUserRepository implements UserRepository{
+    private FILE_PATH = `${__dirname}/users`;
+
+    async save(user: User): Promise<void> {
+        try {
+            let path = this.filePath(user.id);
+            console.log(path);
+            if (fs.existsSync(path)) {
+                this.deleteFile(path);
+            }
+            fs.promises.writeFile(path, serialize(user));
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async search(userId: string): Promise<User> {
+        const userData = await fs.promises.readFile(this.filePath(userId));
+        const { id, email, password } = deserialize(userData);
+        return new User({ id, email, password });
+    }
+
+    private filePath(id: string): string {
+        return `${this.FILE_PATH}.${id}.repo`;
+    }
+
+    deleteFile(urlFile:string) {
+        fs.unlink(urlFile, (err: any) => {
+            if (err) {
+                throw err;
+            }
+        });
+    }
+}
--- test/Context/Register/Infraestructure/Persistence/FileUserRepository.test.ts
+++ test/Context/Register/Infraestructure/Persistence/FileUserRepository.test.ts
@@ -1,5 +1,5 @@
 import { FileUserRepository } from '../../../../../src/Context/Register/Infraestructure/Persistense/FileUserRepository';
-import { User } from "../../../../../src/Context/Register/Domain/User";
+import { User } from '../../../../../src/Context/Register/Domain/User';
 
 describe('FileUserRepository', ()=>{
     it('should save a user', async ()=>{
